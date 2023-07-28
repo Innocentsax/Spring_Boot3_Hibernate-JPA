468c62cab2a6e6190191ff821fed126eb3a87a31
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-07-28 14:42:54 +0100

    Spring Data JPA

---
+++ spring-data-jpa-course/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student udo  = new Student("Innocent", "Udo", "innocentcharlesudo@gmail.com", 25);
+            studentRepository.save(udo);
+        };
+    }
+}
