0564ed73158b3809b3fe92fcdf58c017c73f19e4
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-21 16:14:34 +0100

    JPA Lazy Loading

---
+++ JPA-Advance-Mapping-LAZY-LOADING/src/main/java/dev/Innocent/JPAAdvanceMapping/DAO/AppDAO.java
@@ -0,0 +1,22 @@
+package dev.Innocent.JPAAdvanceMapping.DAO;
+
+import dev.Innocent.JPAAdvanceMapping.Entity.Course;
+import dev.Innocent.JPAAdvanceMapping.Entity.Instructor;
+import dev.Innocent.JPAAdvanceMapping.Entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor thInstructor);
+    Instructor findInstructorById(int theId);
+    void deleteInstructorById(int theId);
+    InstructorDetail findInstructorDetailById(int theId);
+    void deleteInstructorDetailById(int theId);
+    List<Course> findCoursesByInstructorId(int theId);
+    Instructor findInstructorByIdJoinFetch(int theId);
+    void update(Instructor tempInstructor);
+    void update(Course tempCourse);
+    Course findCourseById(int theId);
+    void deleteCourseById(int theId);
+}
